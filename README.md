7b743287af1be9b791da6b7641e357eec66943ca
Anitha <ac875@student.le.ac.uk>
2022-08-11 21:14:09 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Hey do you like socket.io and whiteboards?
+### yeah, but the demo they give you doesn't save anything
+# WELL DO I HAVE THE DEMO FOR YOU!
+## what if I told you that you could make a simple socket.io whiteboard and permanently persist the drawings?
+## well gee how would I do that?
+# WITH MONGODB OF COURSE!
+
+# Setup
+First, install mongoDB using the package manager of your choice. Then fire it up with the default settings.
+
+Clone this repository, and run ```npm install``` to pull in the dependencies (express, socket.io, mongo driver).
+
+Run ```node index.js``` to start the server on port 3000.
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = 3000;
+const mongo = require('mongodb').MongoClient;
+
+mongo.connect('mongodb://ec2-44-204-160-75.compute-1.amazonaws.com:27017/draw', (error, db) => {
+	if (error) throw error;
+	console.log('Connected to DB');
+	app.use(express.static(__dirname + '/public'));
+
+	function onConnection(socket) {
+		console.log('client connected');
+		db.collection('paint').find().sort({timestamp:1}).toArray((error, result) => {
+			socket.emit('init', result);
+			if (error) console.error(error);
+		});
+
+		socket.on('paint', (data) => {
+			data.timestamp = new Date();
+			db.collection('paint').save(data, (error, result) => {
+				if (error) console.log(error);
+			});
+			socket.broadcast.emit('paint', data);
+		});
+		socket.on('erase', (data) => {
+			
+			db.collection('paint').drop( (error, result) => {
+				if (error) console.log(error);
+			});
+			socket.broadcast.emit('erase', data);
+		});
+	}
+
+	io.on('connection', onConnection);
+
+	http.listen(port, () => console.log('running on port ' + port));
+});
